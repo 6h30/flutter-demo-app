db8c6b3bdda70bfb494535babca93ee06c70ebe5
6h30 <truongduc1108@gmail.com>
2026-05-17 04:28:55 +0000

    Fix signing config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,14 +37,13 @@ android {
     }
 
     signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-        }
+    create("release") {
+        keyAlias = keystoreProperties.getProperty("keyAlias")
+        keyPassword = keystoreProperties.getProperty("keyPassword")
+        storeFile = file(keystoreProperties.getProperty("storeFile"))
+        storePassword = keystoreProperties.getProperty("storePassword")
     }
-
+}
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
